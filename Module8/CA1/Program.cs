d8533f9da8dc3e949437ec658c58807886ca73d2
Allison Drake <allisonnicoledrake@gmail.com>
2019-11-14 07:03:41 -0800

    Problems 1 and 2 complete. May still do BONUS option but is not yet done.

---
+++ Module8/CA1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CA1 is reporting on args:");
+            Console.WriteLine();
+            Console.WriteLine("If there is something in args, it will print here " +
+                "or tell you that CA1 has zero args:");
+            if (args.Length == 0) Console.WriteLine("CA1 has zero args.");
+            else
+            {
+                foreach(string s in args)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.WriteLine("Thanks for playing CA1 prints args.");
+        }
+    }
+}
---
+++ Module8/CA2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CA2 is reporting:");
+            Console.WriteLine();
+            int i = 0;
+            while (i < 4)
+            {
+                string userString;
+                Console.WriteLine("Enter string '{0}' of 4: ", (i+1));
+                userString = Console.ReadLine();
+                Console.WriteLine("You entered '{0}'", userString);
+                i++;
+            }
+        }
+    }
+}
